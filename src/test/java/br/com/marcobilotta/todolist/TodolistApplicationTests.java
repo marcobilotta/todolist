f65b9c5add09ecd15c97d9860e11407471324888
Marco Bilotta <marco.bilotta@outlook.com>
2023-10-10 10:07:28 -0300

    feat: start project todolist

---
+++ src/test/java/br/com/marcobilotta/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcobilotta.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
